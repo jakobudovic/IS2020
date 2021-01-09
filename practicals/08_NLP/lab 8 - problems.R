0c457feb50031f2a115854f87ad4632a54273d2f
jakobudovic <ju8168@student.uni-lj.si>
2021-01-09 15:10:27 +0100

    Vaje 8

---
+++ practicals/08_NLP/lab 8 - problems.R
@@ -0,0 +1,29 @@
+##########################################################################################################################
+#
+# PROBLEMS
+#
+##########################################################################################################################
+#
+# - Build a corpus from the documents in the "earn" directory
+#
+# - Load the document classes from "earn-topics.txt"
+#
+# - Construct the data set and split it into the train (70% of instances) and test (30% of instances) subsets
+#
+# - Train several models (KNN, SVM, ANN, ...) and evaluate how successfully they classify test documents
+#
+##########################################################################################################################
+
+
+#######################################
+#
+# - Visualize the low-dimensional representation of the corpus w.r.t. target space.
+# What do you observe?
+#
+#######################################
+
+#######################################
+#
+# Generate the wordcloud by using different pre-processing techniques. Explain the behaviour.
+#
+#######################################
